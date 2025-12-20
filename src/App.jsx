c832d7d52c1f40122914a6c5ee59aa9719e0d846
Amandeep563 <amandeepbollampalli@gmail.com>
2025-12-21 02:03:22 +0530

    refactor(app): Refactor main layout and remove loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,42 @@ import { useEffect } from "react"
 import { motion } from "framer-motion"
 
 const App = () => {
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        const timer = setTimeout(() => setLoading(false), 2000);
-        return () => clearTimeout(timer)
-    }, []);
-
-    if (loading) {
-        return (
-            <div className="h-screen w-full bg-black flex items-center justify-center">
-                <motion.h1
-                    initial={{ scale: 0 }}
-                    animate={{ scale: [1, 1.2, 1] }}
-                    transition={{ repeat: Infinity }}
-                    className="text-yellow-400 font-comic text-6xl italic"
-                >
-                    LOADING...
-                </motion.h1>
-            </div>
-        )
-    }
+    // const [loading, setLoading] = useState(true)
+    //
+    // useEffect(() => {
+    //     const timer = setTimeout(() => setLoading(false), 2000);
+    //     return () => clearTimeout(timer)
+    // }, []);
+    //
+    // if (loading) {
+    //     return (
+    //         <div className="h-screen w-full bg-black flex items-center justify-center">
+    //             <motion.h1
+    //                 initial={{ scale: 0 }}
+    //                 animate={{ scale: [1, 1.2, 1] }}
+    //                 transition={{ repeat: Infinity }}
+    //                 className="text-yellow-400 font-comic text-6xl italic"
+    //             >
+    //                 LOADING...
+    //             </motion.h1>
+    //         </div>
+    //     )
+    // }
     return (
-        <div className="bg-black">
-            <Hero />
-            <Projects />
-            <Contact />
-        </div >
-    )
+        <main className="bg-black">
+            <section id="hero" className="relative h-screen">
+                <Hero />
+            </section>
+
+
+            <section id="projects" className="relative">
+                <Projects />
+            </section>
+
+            <section id="contact" className="relative h-screen">
+                <Contact />
+            </section>
+        </main>)
 }
 
 export default App
