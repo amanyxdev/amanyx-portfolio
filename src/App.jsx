a642808606f0ba4349d33a28cea2abe0e48b72c9
Amandeep563 <amandeepbollampalli@gmail.com>
2025-12-20 20:28:36 +0530

    feat(loading): Add initial loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react"
 import Contact from "./components/Contact"
 import Hero from "./components/Hero"
 import Projects from "./components/Projects"
+import { useEffect } from "react"
+import { motion } from "framer-motion"
 
 const App = () => {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 2000);
+        return () => clearTimeout(timer)
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="h-screen w-full bg-black flex items-center justify-center">
+                <motion.h1
+                    initial={{ scale: 0 }}
+                    animate={{ scale: [1, 1.2, 1] }}
+                    transition={{ repeat: Infinity }}
+                    className="text-yellow-400 font-comic text-6xl italic"
+                >
+                    LOADING...
+                </motion.h1>
+            </div>
+        )
+    }
     return (
-        <div>
+        <div className="bg-black">
             <Hero />
             <Projects />
             <Contact />
